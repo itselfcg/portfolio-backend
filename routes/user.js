0f2d4ffb171d3fdbe5497d753cd67ff55574bd40
Itzel Contreras <itselfcg@gmail.com>
2021-07-24 13:12:12 -0700

    Auth to create users was added

--- routes/user.js
+++ routes/user.js
@@ -1,8 +1,9 @@
 const express = require("express");
 const router = express.Router();
 const UserController = require("../controller/user");
+const checkAuth = require("../middleware/check-auth.js");
 
-router.post("/register", UserController.create);
+router.post("/register",checkAuth, UserController.create);
 
 router.post("/login", UserController.login );
 
